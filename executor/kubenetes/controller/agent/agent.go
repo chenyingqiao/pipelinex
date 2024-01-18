3f95ac37f32950d2d6c9908344727769a5f93c0d
chenyj03 <chenyj03@mingyuanyun.com>
2024-01-18 10:35:34 +0800

    feat: controller 修改

--- executor/kubenetes/controller/agent/agent.go
+++ executor/kubenetes/controller/agent/agent.go
@@ -1,6 +1,8 @@
 package agent
 
 import (
+	"fmt"
+
 	"github.com/chenyingqiao/pipelinex/executor/kubenetes/controller"
 	clientset "github.com/chenyingqiao/pipelinex/executor/kubenetes/generated/clientset/versioned"
 	"github.com/chenyingqiao/pipelinex/executor/kubenetes/generated/clientset/versioned/scheme"
@@ -8,6 +10,8 @@ import (
 	listers "github.com/chenyingqiao/pipelinex/executor/kubenetes/generated/listers/agentcontroller/v1alpha1"
 	"github.com/golang/glog"
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/util/runtime"
 	kuberInformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	kubeScheme "k8s.io/client-go/kubernetes/scheme"
@@ -15,11 +19,21 @@ import (
 	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/record"
 	"k8s.io/client-go/util/workqueue"
-	"k8s.io/apimachinery/pkg/util/runtime"
 )
 
 const controllerAgentName = "agent-controller"
 
+const (
+	// SuccessSynced is used as part of the Event 'reason' when a Bar is synced
+	SuccessSynced = "Synced"
+	// ErrResourceExists is used as part of the Event 'reason' when a Bar fails
+	// to sync due to a Deployment of the same name already existing.
+	ErrResourceExists = "ErrResourceExists"
+
+	// MessageResourceSynced is the message used for an Event fired when a Bar
+	// is synced successfully
+	MessageResourceSynced = "Bar synced successfully"
+)
 // 这个是Agent工作负载编排的控制器
 // 它的功能：
 // - 创建流水线工作负载的Pod, 并保证工作负载的pod创建成功
@@ -73,11 +87,55 @@ func NewController(kubeclientset kubernetes.Interface, agentclientset clientset.
 	return ctr
 }
 
+// 执行控制器功能
 func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
+
 	return nil
 }
+
+func (c *Controller) runWorker() {
+	for {
+		// 获取工作队列里面的对象
+		obj, shutdown := c.workqueue.Get()
+		if shutdown {
+			return
+		}
+		defer c.workqueue.Done(obj)
+		// 判断是否是字符串
+		if _, ok := obj.(string); !ok {
+			c.workqueue.Forget(obj)
+			runtime.HandleError(fmt.Errorf("工作队列中预期的字符串但得到了 %#v", obj))
+			return
+		}
+	}
+}
+
+// processing 处理对应的数据
+func (c *Controller) processing(key string) error {
+	// Convert the namespace/name string into a distinct namespace and name
+	namespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
+		return nil
+	}
+
+	agent, err := c.agentLister.Agents(namespace).Get(name)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			runtime.HandleError(fmt.Errorf("bar '%s' in work queue no longer exists", key))
+			return nil
+		}
+		return err
+	}
+
+	// 处理agent对应的业务逻辑
+
+	c.recorder.Event(agent, corev1.EventTypeNormal, SuccessSynced, MessageResourceSynced)
+	return nil
+}
+
 // enqueue bar 获取一个 bar 资源并将其转换为名称空间/名称 字符串，然后将其放入工作队列中
-//此方法不应该传递除 bar 之外的任何类型的资源。
+// 此方法不应该传递除 bar 之外的任何类型的资源。
 func (c *Controller) enqueueBar(obj interface{}) {
 	var key string
 	var err error
