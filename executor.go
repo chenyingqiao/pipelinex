e38d3712ae05561eda2340f8c3e28803c8279410
chenyj03 <chenyj03@mingyuanyun.com>
2025-11-18 16:17:01 +0800

    feat: 修改执行器interface

--- executor.go
+++ executor.go
@@ -8,16 +8,16 @@ type Executor interface {
 	Prepare(ctx context.Context) error
 	// Destruction 销毁环境
 	Destruction(ctx context.Context) error
+	// Transfer 传输需要执行的数据，并且反回执行的结果
+	Transfer(ctx context.Context, in chan<- any, out <-chan any)
 }
 
 type Adapter interface {
 	// Config 适配器配置
-	Config(config map[string]interface{}) Adapter
-	// Conn 连接到环境中
-	Conn(ctx context.Context) interface{}
+	Config(ctx context.Context, config map[string]any) error
 }
 
 type Bridge interface {
-	// Transfer 传输需要执行的数据，并且反回执行的结果
-	Transfer(ctx context.Context, in chan<- interface{}, out <-chan interface{})
+	// Conn 连接到环境中
+	Conn(ctx context.Context, adapter Adapter) (Executor, error)
 }
